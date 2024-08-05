d98c75a34b66c3bbc48f302a4d45ffa8315ee870
Rahul251202 <rahul251202@gmail.com>
2024-08-05 23:27:10 +0530

    commit

---
+++ app/src/main/java/com/example/bmicalculatorapp/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.bmicalculatorapp
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.example.bmicalculatorapp.databinding.ActivityMainBinding
+private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val t1= binding.bmiResult
+        val weight= binding.editTextweight
+        val height= binding.editTextheight
+        val result_btn=binding.buttonResult
+        result_btn.setOnClickListener {
+            if((weight.text.toString() == "") || (height.text.toString() == "")){
+                Toast.makeText(this, "Enter height and weight properly!", Toast.LENGTH_SHORT).show()
+            }else {
+                val area = binding.resultCv
+                area.isVisible = true
+                val w: String = weight.text.toString()
+                val h: String = height.text.toString()
+                val bmi: Float = w.toFloat() / ((h.toFloat() / 100) * (h.toFloat() / 100))
+                val ubmi = String.format("%.2f", bmi).toFloat()
+                t1.text = ubmi.toString()
+                printStatus(ubmi)
+            }
+
+        }
+
+
+    }
+
+    private fun printStatus(ubmi: Float) {
+        val t2= binding.resultState
+        val t3= binding.range
+        t3.text="18.5-24.9 is healthy range"
+        if(ubmi<16.0){
+            t2.text="you are underweight(severe)."
+            t2.setTextColor(Color.YELLOW)
+
+        }else if(ubmi in 16.0..16.9){
+            t2.text="you are underweight(Moderate)."
+            t2.setTextColor(Color.YELLOW)
+        } else if(ubmi in 17.0..18.4){
+            t2.text="you are underweight(Mild)."
+            t2.setTextColor(Color.YELLOW)
+        }else if(ubmi in 18.5..24.9){
+            t2.text="you are healthy."
+            t2.setTextColor(Color.GREEN)
+        }else if(ubmi in 25.0..29.9){
+            t2.text="you are overweighted."
+            t2.setTextColor(Color.parseColor("#FFC107"))
+        }else if(ubmi in 30.0..34.9){
+            t2.text="you are too much overweighted."
+            t2.setTextColor(Color.parseColor("#FF9800"))
+        }else if(ubmi in 35.0..39.9){
+            t2.text="you are extremely overweighted."
+            t2.setTextColor(Color.parseColor("#FF5722"))
+        }else{
+            t2.text="you are at health risk(OverWeighted)."
+            t2.setTextColor(Color.parseColor("#F8DC2619"))
+        }
+
+
+    }
+}
